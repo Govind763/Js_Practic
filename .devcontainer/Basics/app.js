aed0261a6448b9bf8198b866d9ab6bfa757f919a
GOVIND JAISWAL <72164914+Govind763@users.noreply.github.com>
2024-09-29 06:59:24 +0000

    comparison

--- .devcontainer/Basics/app.js
+++ .devcontainer/Basics/app.js
@@ -31,6 +31,31 @@ console.table([a,k,p,t])
 // console.log(typeof(valueInNumber))  -->number
 // console.log(valueInNumber)  --> NaN
 
+//
+// console.log("1" + 2); -->12
+// console.log(1 + "2"); -->12
+// console.log("1" + 2 + 2); -->122
+// console.log(1 + 2 + "2"); -->32
 
+//
+// console.log(true) -->true
+// console.log (+true) --> 1
+// console.log(true+);  -->error
+// console.log(+""); -->0
 
+// let num1, num2, num3
+// num1 = num2 = num3 = 3+3
+// console.log(num2)
 
+// comparison 
+
+//  console.log("2" >1); -->true
+//  console.log("02" >1); -->true
+
+// console.log(null >0) -->true
+// console.log(null == 0) -->fale
+// console.log(null >= 0); -->true
+// this is bcz equality check == and comaprisons ><= >= work differently 
+// Comparison covert the null to a number, teating as 0
+
+// strict check === aslo check datatype
