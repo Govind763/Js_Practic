950e5831a64448930b5db45be66c6bfbacad6559
GOVIND JAISWAL <72164914+Govind763@users.noreply.github.com>
2024-09-29 06:30:00 +0000

    conversion of datatype

--- .devcontainer/Basics/app.js
+++ .devcontainer/Basics/app.js
@@ -1,1 +1,36 @@
+// --> we can use 1. node.js 2. Deno for giving the environment 
 console.log("Hii Everyone !!!")
+var a="Govind"
+let k=3434;
+const p =2;
+t=343
+//another way of printing the values
+console.table([a,k,p,t])
+
+//primitive data type
+// 1. number -> 2 to power 53
+// 2. biging
+// string -> ""
+// boolean -> true/false
+// null -> standalone value
+// symbol -> uniquness
+
+
+//object
+// console.log(typeof null); ->> object 
+// console.log(typeof undefined) --> undefined
+// console.log(typeof(undefined)) --> same will come 
+
+
+// CONVERSION 
+// let score = "33ab" -->{NaN}
+// let score = null --> {0}
+// let score = undefined --> {NaN}
+// let score = true --> {1}
+// let valueInNumber = Number(score)   -->typecasting
+// console.log(typeof(valueInNumber))  -->number
+// console.log(valueInNumber)  --> NaN
+
+
+
+
